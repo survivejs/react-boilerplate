f65d3129e251936c44aed6d8ff51cb946ec42f87
Juho Vepsalainen <bebraw@gmail.com>
2016-06-23 13:52:39 +0300

    tests - Preprocess through coverage and output in multiple formats

--- karma.conf.js
+++ karma.conf.js
@@ -30,7 +30,7 @@ module.exports = function karmaConfig (config) {
       // Reference: http://webpack.github.io/docs/testing.html
       // Reference: https://github.com/webpack/karma-webpack
       // Convert files with webpack and load sourcemaps
-      'tests/**/*_test.*': ['webpack', 'sourcemap']
+      'tests/**/*_test.*': ['webpack', 'sourcemap', 'coverage']
     },
 
     browsers: [
@@ -42,8 +42,23 @@ module.exports = function karmaConfig (config) {
 
     // Configure code coverage reporter
     coverageReporter: {
-      dir: 'coverage/',
-      type: 'html'
+      reporters: [
+          // generates ./coverage/lcov.info
+          {
+            type: 'lcovonly',
+            subdir: '.'
+          },
+          // generates ./coverage/coverage-final.json
+          {
+            type: 'json',
+            subdir: '.'
+          },
+          // generates ./coverage/index.html
+          {
+            type: 'html',
+            subdir: '.'
+          }
+      ]
     },
 
     // Test webpack config
